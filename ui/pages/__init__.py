3bf18785e591a08450b87af4fda37765d6e57f55
Samama Karim <154582050+SamamaKarim092@users.noreply.github.com>
2025-12-12 19:46:27 +0500

    Create __init__.py

---
+++ ui/pages/__init__.py
@@ -0,0 +1,10 @@
+"""
+Pages package initialization.
+"""
+
+from .home import HomePage
+from .predict import PredictPage
+from .upload import UploadPage
+from .charts import ChartsPage
+from .reports import ReportsPage
+from .settings import SettingsPage
