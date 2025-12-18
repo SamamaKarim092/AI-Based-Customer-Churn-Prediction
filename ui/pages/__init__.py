e86906f274ea47523b3fd58a600caf523e42d1e0
Samama Karim <154582050+SamamaKarim092@users.noreply.github.com>
2025-12-18 21:12:18 +0500

    Update __init__.py

--- ui/pages/__init__.py
+++ ui/pages/__init__.py
@@ -5,6 +5,8 @@ Pages package initialization.
 from .home import HomePage
 from .predict import PredictPage
 from .upload import UploadPage
+from .upload_result import UploadResultPage
 from .charts import ChartsPage
 from .reports import ReportsPage
 from .settings import SettingsPage
+
